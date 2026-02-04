24905dffe9dbb661a532a9fe5bde022667e247c3
brunojjsa <72888008+brunojjsa@users.noreply.github.com>
2026-02-04 11:58:26 -0300

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["ec2-15-229-244-18.sa-east-1.compute.amazonaws.com"];
+pub const RS_PUB_KEY: &str = "eWaE72YjSwuLnbXa8IgsGhIHsJJLGrlMWdTOQMAe6mM";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
