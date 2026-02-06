c92d7c26eb9f839e22e5eebf02facbc0da85f7e1
SKYWK0913 <71912650@qq.com>
2026-02-06 14:52:21 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["36.32.187.31"];
+pub const RS_PUB_KEY: &str = "SBPnTSanC74zLGdIxogK9r9YoA83flGj5pZQuJIlWWo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
