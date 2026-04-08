875d4eeb80d38f681d0f41a60222e936c4bae15b
qgg <117129098+qq3071305@users.noreply.github.com>
2026-04-08 23:43:40 +0800

    Update rendezvous server and public key constants

--- src/config.rs
+++ src/config.rs
@@ -156,8 +156,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["w13.xyz:21126"];
+pub const RS_PUB_KEY: &str = "3Al01OYO2KIHBsAXy1MPwupIdGy5kKK1X6R9NmevfRw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
