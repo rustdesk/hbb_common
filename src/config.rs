e08c6ac7771e4abeb2ad5d17d1faf4bd2c6b6fb2
leegeecn <pubyou2001@qq.com>
2026-04-17 12:52:14 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -156,8 +156,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.94.86.95"];
+pub const RS_PUB_KEY: &str = "UMahCEEQ3uQGMLc3swy2C7GNHzhxO1MwbOLVZJm2Alg=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
