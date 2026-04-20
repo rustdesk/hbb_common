cefbbc196f81ca9a5802c33411c253a14ef9678a
dc852827151 <852827151@qq.com>
2026-04-20 16:25:55 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -156,8 +156,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.12.54"];
+pub const RS_PUB_KEY: &str = "LwGoAc2iK3FKzqgWxAfHexlbdXO1+Byzb3h6A2ITdNM=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
