67dd4eb2c3ef192a8e81405c007f38c8fc58f36b
775282537 <775282537@qq.com>
2026-05-20 19:35:01 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -156,8 +156,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.csoc.cc"];
+pub const RS_PUB_KEY: &str = "GjUAYAMXaBol4nyXRc6ZAwaTRB3bDmn24103lEh0bVM=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
