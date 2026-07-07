fccf22268ff6872bcc5f4eb786c964f2a65b2ed3
Marcos Roberto Lira <marcosgja93@gmail.com>
2026-07-07 12:14:39 -0300

    Change rendezvous server and public key values
    Updated rendezvous server address and public key.

--- src/config.rs
+++ src/config.rs
@@ -117,8 +117,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: "remoto2.grupoliratec.org";
+pub const RS_PUB_KEY: &str = "I74lBiZ8FDPUDkjfTzrGYJnOkvRaMPz4o+tnm1aVPXg";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
