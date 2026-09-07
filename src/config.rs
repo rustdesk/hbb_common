2a060e8f67c4dbbdb4236570af4957f1f1569063
wuxixihuosheng <44336077+wuxixihuosheng@users.noreply.github.com>
2026-09-07 09:27:50 +0800

    Update rendezvous server and public key values

--- src/config.rs
+++ src/config.rs
@@ -114,8 +114,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["175.27.165.55"];
+pub const RS_PUB_KEY: &str = "omQCSJfCMsYQjGpyAbpIcgyHGne4mkbxJsfNmKRs5EY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
