ea1ce97365523031d920c85bdf303dd41bb9cb74
nodetao <112098578+nodetao@users.noreply.github.com>
2026-09-07 16:07:21 +0800

    更改提交

--- src/config.rs
+++ src/config.rs
@@ -114,8 +114,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["naitoab.com"];
+pub const RS_PUB_KEY: &str = "bGO5gzI72WvMfaDVJBjoqVRO+GQ7xi50OszZWKuyYuQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
